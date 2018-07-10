62923422785411ee26a7e9deb29e0ad080e86ae4
jaivonmassena <jaivonmassena@gmail.com>
2018-07-10 22:39:16 +0200

    removed canvas drawing and  began working on project

--- projects/graph/src/App.js
+++ projects/graph/src/App.js
@@ -3,8 +3,10 @@ import { Graph } from './graph';
 import './App.css';
 
 // !!! IMPLEMENT ME
-// const canvasWidth = 
-// const canvasHeight = 
+const canvasWidth = 1000;
+const canvasHeight = 900;
+const circleRadius = 15;
+
 
 /**
  * GraphView
@@ -29,11 +31,106 @@ class GraphView extends Component {
    */
   updateCanvas() {
     let canvas = this.refs.canvas;
-    let ctx = canvas.getContext('2d');
-    
-    // Clear it
-    ctx.fillStyle = 'white';
-    ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+   let ctx = canvas.getContext('2d');
+
+   console.log('this.props.graph: ', this.props.graph);
+   // call our dummy function
+   this.props.graph.createDummyGraph();
+   console.log('called createDummyGraph');
+
+   // Clear it
+   ctx.fillStyle = 'grey';
+   ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+
+   ctx.font = '13px Arial';
+   ctx.textAlign = 'center';
+   ctx.textBaseline = 'middle';
+
+   // draw our dummy vertexes
+   this.props.graph.vertexes.forEach((v) => {
+     ctx.beginPath();
+     ctx.fillStyle = 'white';
+     ctx.arc(v.pos.x, v.pos.y, circleRadius, 0, 2 * Math.PI);
+     ctx.fill();
+     ctx.stroke();
+
+     // fill in the text
+     ctx.fillStyle = 'black';
+     ctx.fillText(v.value, v.pos.x, v.pos.y);
+   }); 
+    // let canvas = this.refs.canvas;
+    // let ctx = canvas.getContext('2d');
+    //
+    // // Clear it
+    // ctx.fillStyle = 'black';
+    // ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+    // let x = 300;
+    // let y = 197;
+    //
+    // for(let i = 0; i <= 5; i++)
+    // {
+    //
+    //   ctx.fillStyle = 'red';
+    //   ctx.fillRect( x, 100, 40,  40);
+    //   x+= 43;
+    //   y+= 10;
+    // }
+    // x = 257;
+    // for(let i = 0; i <= 9; i++){
+    //
+    //   ctx.fillStyle = 'red';
+    //   ctx.fillRect( x, 142, 40,  40);
+    //   x+= 43;
+    // }
+    // x= 256;
+    // for(let i = 0; i <= 7; i++){
+    //   if(i === 0 || i === 2 || i === 6){
+    //     ctx.fillStyle = 'green';
+    //   }else{
+    //   ctx.fillStyle = 'orange';}
+    //   ctx.fillRect(x, 183, 40, 40);
+    //
+    //   x+=43;
+    // }
+    // x = 213;
+    // for(let i = 0; i <= 10; i++){
+    //   if(i === 0 || i === 3 || i === 7){
+    //     ctx.fillStyle = 'green';
+    //   }else{
+    //   ctx.fillStyle = 'orange';}
+    //
+    //   ctx.fillRect( x, 225, 40,  40);
+    //   x+= 43;
+    // }
+    // x = 213;
+    // for(let i = 0; i <= 11; i++){
+    //
+    //   if(i === 0 || i === 2 || i === 8){
+    //     ctx.fillStyle = 'green';
+    //   }else{
+    //   ctx.fillStyle = 'orange';}
+    //
+    //   ctx.fillRect( x, 267, 40,  40);
+    //   x+= 43;
+    // }
+    // x = 213;
+    // for(let i = 0; i <= 10; i++){
+    //
+    //   if(i === 0 || i === 1 || i === 7 || i === 8 || i === 9 || i === 10){
+    //     ctx.fillStyle = 'green';
+    //   }else{
+    //   ctx.fillStyle = 'orange';}
+    //
+    //   ctx.fillRect( x, 310, 40,  40);
+    //   x+= 43;
+    // }
+    // x = 300;
+    // for(let i = 0; i <= 7; i++){
+    //
+    //   ctx.fillStyle = 'orange';
+    //   ctx.fillRect( x, 353, 40,  40);
+    //   x+= 43;
+    // }
 
     // !!! IMPLEMENT ME
     // compute connected components
@@ -41,7 +138,7 @@ class GraphView extends Component {
     // draw verts
     // draw vert values (labels)
   }
-  
+
   /**
    * Render
    */
